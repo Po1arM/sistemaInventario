8cfd34c863ad17e9a7ea15207acd7fe280403a31
Felix <felix1020304@gmail.com>
2022-06-21 17:17:46 -0400

    update routes

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -12,11 +12,14 @@ const Order = require('./models/order')
 const OrderDetails = require('./models/orderDetails')
 const Supplier = require('./models/supplier')
 
+//Listar todos los componentes
 router.get('/components/list', async(req,res)=>{
     const components = await Component.find()
     res.send(components)
 })
 
+//Listar un componente dado un id
+//Tambien lista su relacion con tiempo de entrega
 router.get('/components/:id', async(req,res)=>{
     const {id} = req.params
     const aggregate = [
@@ -34,12 +37,32 @@ router.get('/components/:id', async(req,res)=>{
     res.send(component)
 })
 
+//Modifica un componente
+/*Body
+ * codComponent: cod,
+    description: description,
+    unit: unit,
+    store:[{
+        storeCode: storeCode,
+        storeBalance: amount
+    }]
+ */
 router.put('/components/:id', async(req,res)=>{
     const {id} = req.params
     await Component.update({codComponent:id},req.body)
     res.send(component)
 })
 
+//Crear un componente
+/*Body
+ * codComponent: cod,
+    description: description,
+    unit: unit,
+    store:[{
+        storeCode: storeCode,
+        storeBalance: amount
+    }]
+ */
 router.post('/components', async(req,res)=>{
     const {codComponent} = req.body
     var exists = await Component.exists({codComponent:codComponent})
@@ -51,17 +74,26 @@ router.post('/components', async(req,res)=>{
     res.send('Error! Producto ya existe')
 })
 
+//Crear un nuevo suplidor
+/*codSupplier: codSupplier,
+    name: name,
+    rnc: rnc,
+    city: city,
+    address: address
+     */
 router.post('/suppliers', async(req,res)=>{
     const supplier = new Supplier(req.body)
     await supplier.save()
     res.send(component)
 })
 
+//Listar los suplidores
 router.get('/suppliers/list', async(req,res)=>{
     const suppliers = await Supplier().find()
     res.send(suppliers)
 })
 
+//Buscar un suplidor y los componentes que entrega
 router.get('/suppliers/:id', async(req,res)=>{
     const {id} = req.params
     const aggregate = [
@@ -79,6 +111,8 @@ router.get('/suppliers/:id', async(req,res)=>{
     res.send(supplier)
 })
 
+//Registrar el tiempo de envio para un suplidor y un componente
+//Si ya existe, entonces se actualiza
 router.post('/deliveryTime', async(req,res)=>{
     const {codSupplier} = req.body
     const {codComponent} = req.body
@@ -92,23 +126,108 @@ router.post('/deliveryTime', async(req,res)=>{
     res.send(delivery)
 })
 
+//Listar todos los tiempos de envio
 router.post('/inventoryMovement', async(req,res)=>{
     const inventoryMovement = new InventoryMove(req.body)
     await inventoryMovement.save()
     res.send(inventoryMovement)
 })
 
+//Listar todos los movimientos de inventario
 router.get('/inventoryMovement', async(req,res)=>{
     const inventoryMovements = await InventoryMove.find()
     res.send(inventoryMovements)
 })
 
+//Listar todas las ordenes
 router.get('/order', async(req,res)=>{
-    const ordenes = await Order.find()
-    res.send(ordenes)
+    const orders = await Order.find()
+    res.send(orders)
 })
 
-router.post('/order',async(req,res)=>{
-    //Alogritmos para generar orden
+//Listar las ordenes en un rango determinado
+router.get('/order/inRange', async(req,res)=>{
+    const {fechaInicio,fechaFin} = req.body
+    const aggregate = [
+        {$match:{
+            orderDate:{
+            $gte: ISODate(fechaInicio),
+            $lt: ISODate(fechaFin) 
+        }
+        }},
+        {$lookup:{
+            from: "Orders",
+            localField: "orderNumber",
+            foreignField:"orderNumber",
+            as: "orderDetails"
+        }
+
+        }]
+    const orders = await Order.aggregate(aggregate)
+    res.send(orders)
 })
+
+//Listar las ordenes para un suplidor en especifico
+router.get('/order/supplier/:id', async(req,res)=>{
+    const {id} = req.params
+    const aggregate = [
+        {$match:{
+            orderDate:{
+            $gte: ISODate(fechaInicio),
+            $lt: ISODate(fechaFin) 
+        }
+        }},
+        {$lookup:{
+            from: "Orders",
+            localField: "orderNumber",
+            foreignField:"orderNumber",
+            as: "orderDetails"
+        }
+        }]
+    const orders = await Order.aggregate(aggregate)
+    res.send(orders)
+})
+
+//Crear la orden de manerea automatica
+router.post('/order',async(req,res)=>{
+    var { orderDate, components} = req.body;
+    orderDate = new Date(orderDate);
+    try {
+        var suplidores = []
+        for (i = 0; i < articulosNecesarios.length; i++) {
+            aux = await suplidorCercano(articulosNecesarios[i].component, orderDate)
+            suplidores.push(aux);
+        }
+
+        var articuloSuplidor = []
+        for (i = 0; i < suplidores.length; i++) {
+            var suplidor;
+            for (j = 0; j < articulosNecesarios.length; j++) {
+                if (suplidor === null) 
+                    suplidor = suplidores[i].codSupplier
+                
+                auxArticulo = {
+                    codComponent: articulosNecesarios[j].component,
+                    amount: articulosNecesarios[j].amount,
+                    price: articulosNecesarios[j].precio,
+                }
+                articuloSuplidor.push(auxArticulo);
+                
+            }
+            const newOrder = new Order({
+            detailCod: detailCod,
+            codSupplier: suplidor,
+            orderDate: orderDate,
+            })
+            await newOrder.save();
+            numeroOrden++;
+            articuloSuplidor = [];
+        
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router
